666ef2a99bf76afdbe0fdb8d20cc430d1ae16612
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-06-13 14:36:40 -0400

    adding deleting a friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,4 +82,41 @@ const userController = {
   },
 
   //Adding A friend
+
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $addToSet: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //Deleting a friend
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No User Found With This Id!!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
+
+module.exports = userController;
