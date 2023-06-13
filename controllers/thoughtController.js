9dabaf4d7b054d46bfddc8a4423de6478c1a57b8
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-06-13 13:38:53 -0400

    commiting create all thoughts, update by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,6 +1,6 @@
 const { Thought, User} = require ("../models");
-const { db } = require("../models/User");
 
+//get all thoughts
 const thoughtController = {
     getThoughts(req, res) {
         Thought.find({})
@@ -16,7 +16,7 @@ const thoughtController = {
             res.sendStatus(400);
         });
 },
-
+//get thought by id
 getThoughBuyId({ params
 }, res) {
     Thought.findOne({_id:params.id})
@@ -29,7 +29,44 @@ getThoughBuyId({ params
     if (!dbThoughtData){
         return res.status(404).json({message: "no thought found with this id!"});
     }
-    res.josn(dbThoughtData);
+    res.json(dbThoughtData);
 });
 
+},
+//create thought
+createThought({params, body}, res){
+    Thought.create(body)
+    .then(({_id}) => {
+        return User.findOneAndUpdate (
+            { _id: body.userId},
+            {$push: {thoughts: _id}},
+            { new:true}
+    );
+    })
+    .then((dbUserData) => {
+        if (!dbUserData){
+            return res.status(404).json({message: 'thought created but no user with this id!!'});
+
+
+        }
+        res.json({message: "thought created successfully!!"})
+    })
+    .catch((err) => res.json(err));
+},
+
+//update thought by id
+updateThought({ params, body}, res) {
+    Thought.findOneAndUpdate({_id: params.id}, body, {
+        new:true,
+        runValidators: true,
+    })
+    .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: "No thought found with this id!" });
+            return;
+        } 
+         res.json(dbThoughtData);
+    })
+  
+    .catch((err) => res.json(err));
 },
